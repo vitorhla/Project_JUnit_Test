347ca5bda7ac571ce45bbfece9edb782d9d92a94
vitorhla <vitorhenriquealt@gmail.com>
2021-05-29 08:38:43 -0300

    finalized account initiated Account test

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -2,4 +2,60 @@ package entities;
 
 public class Account {
 
+	private Long id;
+	private Double balance;
+	
+	
+	public static  Double DEPOSIT_FEE_PERCENTAGE = 0.02;
+	
+	public Account() {
+		
+	}
+
+	public Account(Long id, Double balance) {
+		super();
+		this.id = id;
+		this.balance = balance;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+	
+	public void deposit (double amount) {
+		
+		if(amount >0) {
+		
+		//amount = amount - amount *  DEPOSIT_FEE_PERCENTAGE;
+		//balance = balance + amount;
+		amount -= amount *  DEPOSIT_FEE_PERCENTAGE;
+		balance += amount;
+		}
+	}
+	
+	public void withdraw (double amount) {
+		if(amount > balance) {
+			throw new IllegalArgumentException();
+		}
+		
+		balance -= amount;
+	}
+	
+	public double fullWithdraw() {
+		double aux =  balance;
+		balance = 0.0;
+		return aux;
+		
+		
+	}
+	
+	
 }
---
+++ src/tests/entities/AccountTests.java
@@ -0,0 +1,19 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+
+public class AccountTests {
+
+	@Test
+	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+		
+		double amount = 200.0;
+		double expectedValue = 196.0;
+		Account acc = new Account(1L, 0.0);
+		
+	}
+	
+	
+}
