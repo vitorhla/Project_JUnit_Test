07f42f3e0bdd632ddc82bb9e5374fc47c634d795
vitorhla <vitorhenriquealt@gmail.com>
2021-05-29 09:17:33 -0300

    Method Factory created

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import entities.Account;
+import test.factory.AccountFactory;
 
 public class AccountTests {
 
@@ -12,7 +13,7 @@ public class AccountTests {
 		
 		double amount = 200.0;
 		double expectedValue = 196.0;
-		Account acc = new Account(1L, 0.0);
+		Account acc = AccountFactory.createEmptyAccount();
 		
 		acc.deposit(amount);
 		
@@ -23,7 +24,7 @@ public class AccountTests {
 	public void depositShouldDoNothingWhenNegativeAmount() {
 		
 		double expectedValue = 100.0;
-		Account acc = new Account(1L, expectedValue);
+		Account acc = AccountFactory.createAccount(expectedValue);
 		double amount  = -200.0;
 		
 		acc.deposit(amount);
